3a7faf06a9d5aa35beed3607533f9a105c07ab39
RakibHossen678 <rh491464@gmail.com>
2024-06-03 20:51:41 +0600

    fetch user data and showed in tabular format

--- src/pages/Dashboard/ManageUsers.jsx
+++ src/pages/Dashboard/ManageUsers.jsx
@@ -1,10 +1,69 @@
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
 
 const ManageUsers = () => {
-    return (
-        <div>
-            
+  const axiosSecure = useAxiosSecure();
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const { data } = await axiosSecure("/users");
+      return data;
+    },
+  });
+  console.log(users);
+  return (
+    <div>
+      <div className="text-center">
+        <h1 className="text-4xl font-semibold py-9">Manage Users</h1>
+      </div>
+      <div className="flex justify-center">
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th></th>
+                <th>User Name</th>
+                <th>User Email</th>
+                <th>Membership</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, idx) => (
+                <tr key={idx}>
+                  <th>{idx + 1}</th>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="mask mask-squircle w-12 h-12">
+                          <img
+                            referrerPolicy="no-referrer"
+                            src={user?.image}
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <div className="font-bold ">{user?.name}</div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>{user?.email}</td>
+                  <td className="uppercase"> {user?.badge}</td>
+                  <th>
+                    <button className=" bg-green-400  rounded-lg px-4 py-1">
+                      Make Admin
+                    </button>
+                  </th>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ManageUsers;
