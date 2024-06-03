450e92a6a4977123468bbc6a6f137ecbcf599159
RakibHossen678 <rh491464@gmail.com>
2024-06-03 22:18:50 +0600

    added announcement page form

--- src/pages/Dashboard/Announcement.jsx
+++ src/pages/Dashboard/Announcement.jsx
@@ -1,10 +1,68 @@
-
 const Announcement = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div className="w-6/12 mx-auto items-center flex-col">
+      <div className="text-center">
+        <h1 className="text-4xl mt-14 pb-6">Add Announcement</h1>
+      </div>
+      <div>
+        <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+          <form>
+            <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+              <div>
+                <label className="text-gray-700 dark:text-gray-200">
+                  Author Name
+                </label>
+                <input
+                  id="name"
+                  type="text"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+
+              <div>
+                <label className="text-gray-700 dark:text-gray-200">
+                  Author Image
+                </label>
+                <input
+                  id="image"
+                  type="text"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+
+              <div>
+                <label className="text-gray-700 dark:text-gray-200">
+                  Title
+                </label>
+                <input
+                  id="title"
+                  type="text"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+
+              <div>
+                <label className="text-gray-700 dark:text-gray-200">
+                  Description
+                </label>
+                <input
+                  id="description"
+                  type="text"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+                />
+              </div>
+            </div>
+
+            <div className="flex justify-center mt-6">
+              <button className="px-8 py-2.5 leading-5 text-white transition-colors duration-300 transform bg-green-500 rounded-md focus:outline-none ">
+                Make Announcement
+              </button>
+            </div>
+          </form>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default Announcement;
