b37c21322965dedee665cf009acb81097e06860d
RakibHossen678 <rh491464@gmail.com>
2024-06-03 22:44:55 +0600

    save announcement data in database

--- src/pages/Dashboard/Announcement.jsx
+++ src/pages/Dashboard/Announcement.jsx
@@ -1,4 +1,35 @@
+import { useForm } from "react-hook-form";
+import useAuth from "../../Hooks/useAuth";
+import { useMutation } from "@tanstack/react-query";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import toast from "react-hot-toast";
+
 const Announcement = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { register, handleSubmit } = useForm();
+  const { mutateAsync } = useMutation({
+    mutationFn: async (announcementData) => {
+      const { data } = await axiosSecure.post(
+        "/announcement",
+        announcementData
+      );
+      return data;
+    },
+    onSuccess: () => {
+      toast.success("Announcement added successfully");
+    },
+  });
+  const onSubmit = async (data) => {
+    const announcementData = {
+      authorName: data.name,
+      authorImage: data.image,
+      title: data.title,
+      description: data.description,
+    };
+    await mutateAsync(announcementData);
+    console.log(announcementData);
+  };
   return (
     <div className="w-6/12 mx-auto items-center flex-col">
       <div className="text-center">
@@ -6,7 +37,7 @@ const Announcement = () => {
       </div>
       <div>
         <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
               <div>
                 <label className="text-gray-700 dark:text-gray-200">
@@ -14,7 +45,9 @@ const Announcement = () => {
                 </label>
                 <input
                   id="name"
+                  defaultValue={user?.displayName}
                   type="text"
+                  {...register("name", { required: true })}
                   className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 />
               </div>
@@ -26,6 +59,8 @@ const Announcement = () => {
                 <input
                   id="image"
                   type="text"
+                  defaultValue={user?.photoURL}
+                  {...register("image", { required: true })}
                   className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 />
               </div>
@@ -37,6 +72,7 @@ const Announcement = () => {
                 <input
                   id="title"
                   type="text"
+                  {...register("title", { required: true })}
                   className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 />
               </div>
@@ -48,6 +84,7 @@ const Announcement = () => {
                 <input
                   id="description"
                   type="text"
+                  {...register("description", { required: true })}
                   className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
                 />
               </div>
