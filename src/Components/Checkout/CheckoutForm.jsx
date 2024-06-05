b4af7a315af9f12e316119e53d21f049844801cd
RakibHossen678 <rh491464@gmail.com>
2024-06-05 11:02:50 +0600

    first payment done

--- src/Components/Checkout/CheckoutForm.jsx
+++ src/Components/Checkout/CheckoutForm.jsx
@@ -1,21 +1,41 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
 import "./Checkout.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import useAxiosSecure from "../../Hooks/useAxiosSecure";
+import useAuth from "../../Hooks/useAuth";
 
 const CheckoutForm = ({ setIsOpen }) => {
   const stripe = useStripe();
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
   const elements = useElements();
   const [clientSecret, setClientSecret] = useState("");
-
+  const [cardError, setCardError] = useState();
+  const [processing, setProcessing] = useState(false);
+  console.log(clientSecret);
+  useEffect(() => {
+    getClientSecret();
+  }, []);
+  const getClientSecret = async () => {
+    try {
+      const { data } = await axiosSecure.post("/create-payment-intent", {
+        price: 20,
+      });
+      setClientSecret(data.clientSecret);
+    } catch (error) {
+      console.error("Error fetching client secret:", error);
+    }
+  };
   const handleSubmit = async (event) => {
     event.preventDefault();
-
+    setProcessing(true);
     if (!stripe || !elements) {
       return;
     }
     const card = elements.getElement(CardElement);
 
     if (card == null) {
+      setProcessing(false);
       return;
     }
 
@@ -26,41 +46,82 @@ const CheckoutForm = ({ setIsOpen }) => {
 
     if (error) {
       console.log("[error]", error);
+      setCardError(error.message);
+      setProcessing(false);
+      return;
     } else {
       console.log("[PaymentMethod]", paymentMethod);
+      setCardError();
     }
+
+    // confirm payment
+    const { error: confirmError, paymentIntent } =
+      await stripe.confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+          billing_details: {
+            email: user?.email,
+            name: user?.displayName,
+          },
+        },
+      });
+    if (confirmError) {
+      console.log(confirmError.message);
+      setCardError(confirmError.message);
+      setProcessing(false);
+      return;
+    }
+    if (paymentIntent.status === "succeeded") {
+      console.log(paymentIntent);
+      const paymentInfo = {
+        email: user?.email,
+        transactionId: paymentIntent.id,
+      };
+      console.log(paymentInfo);
+    }
+    setProcessing(false);
+    setIsOpen(false);
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement
-        options={{
-          style: {
-            base: {
-              fontSize: "16px",
-              color: "#424770",
-              "::placeholder": {
-                color: "#aab7c4",
+    <>
+      <form onSubmit={handleSubmit}>
+        <CardElement
+          options={{
+            style: {
+              base: {
+                fontSize: "16px",
+                color: "#424770",
+                "::placeholder": {
+                  color: "#aab7c4",
+                },
+              },
+              invalid: {
+                color: "#9e2146",
               },
             },
-            invalid: {
-              color: "#9e2146",
-            },
-          },
-        }}
-      />
-      <div className="flex justify-between items-center gap-4">
-        <button
-          className="bg-green-400 px-3 py-1 rounded-md" 
-          type="submit"
-          disabled={!stripe}
-          onClick={() => setIsOpen(false)}
-        >
-          Pay
-        </button>
-        <button className="bg-red-400 px-3 py-1 rounded-md"  onClick={() => setIsOpen(false)}>Cancel</button>
-      </div>
-    </form>
+          }}
+        />
+        <div className="flex justify-between items-center gap-4">
+          <button
+            // onClick={() => setIsOpen(false)}
+            className="bg-green-400 px-3 py-1 rounded-md"
+            type="submit"
+            disabled={!stripe || !clientSecret || processing}
+          >
+            Pay
+          </button>
+          <button
+            type="button"
+            className="bg-red-400 px-3 py-1 rounded-md"
+            onClick={() => setIsOpen(false)}
+          >
+            Cancel
+          </button>
+        </div>
+      </form>
+      {cardError && <p className="text-red-500 ">{cardError}</p>}
+    </>
   );
 };
 export default CheckoutForm;
